102ee153d775c21bfd29780ff57b7a70d776d587
unknown <edwi02@gmail.com>
2018-01-15 13:34:53 +0100

    se incluye gnModel y ngSubmit

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent {
+
+  constructor() { }
+
+  guardar(){
+    console.log("Formulario posteados")
+  }
+
+}
