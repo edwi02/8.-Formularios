15461d03671ba4436ec8f7ed0ea82fb73c8dfeef
unknown <edwi02@gmail.com>
2018-01-18 12:00:55 +0100

    Carga y Reset de datos al formulario

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -16,13 +16,32 @@ export class DataComponent {
       nombre: "Edwin",
       apellido: "Quintero"
     },
-    correo: "edwin@gmailc.com"
+    correo: "edwin@gmail.com"
 
   }
 
   constructor() {
 
     console.log(this.usuario);
+
+    //Cargar los datos del Usuario en el formulario (Opcion 1).
+    //Nota: Esta opción requiere que sea tusuario:any
+    // this.forma = new FormGroup({
+    //
+    //   'nombreCompleto': new FormGroup({
+    //     'nombre': new FormControl( this.usuario.nombreCompleto.nombre,
+    //                                  [
+    //                                     Validators.required,
+    //                                     Validators.minLength(3)
+    //                                   ] ),
+    //     'apellido': new FormControl(this.usuario.nombreCompleto.apellido, Validators.required)
+    //   }),
+    //   'correo': new FormControl(this.usuario.correo,
+    //                                [
+    //                                   Validators.required,
+    //                                   Validators.pattern('^[a-zA-Z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    //                                 ])
+    // });
 
     this.forma = new FormGroup({
 
@@ -32,18 +51,40 @@ export class DataComponent {
                                         Validators.required,
                                         Validators.minLength(3)
                                       ] ),
-        'apellido': new FormControl('', Validators.required)
+        'apellido': new FormControl('',
+                                        Validators.required)
       }),
-      'correo': new FormControl('', [
+      'correo': new FormControl('',
+                                   [
                                       Validators.required,
                                       Validators.pattern('^[a-zA-Z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
                                     ])
     });
+
+    //Cargar los datos del objeto en el formulario 8Opción 2)
+    this.forma.setValue( this.usuario );
   }
 
   guardarCambios(){
     console.log( this.forma.value );
     console.log( this.forma );
+
+    //Opción 1 para restablecer valores
+    //this.forma.reset( this.usuario );
+
+    //opción 2
+    this.forma.reset({
+      nombreCompleto:{
+        nombre:"",
+        apellido:""
+      },
+      correo:""
+    });
+
+    //Opción 3
+    //this.forma.controls['correo'].reset();
+
+
   }
 
 }
