8fedcd50e8da77b11c14769e661708e31db839a8
unknown <edwi02@gmail.com>
2018-01-18 11:45:56 +0100

    Formulario validado desde el Template o la Data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma:FormGroup;
 
-  ngOnInit() {
+  usuario:Object = {
+
+    nombreCompleto: {
+      nombre: "Edwin",
+      apellido: "Quintero"
+    },
+    correo: "edwin@gmailc.com"
+
+  }
+
+  constructor() {
+
+    console.log(this.usuario);
+
+    this.forma = new FormGroup({
+
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl( '',
+                                     [
+                                        Validators.required,
+                                        Validators.minLength(3)
+                                      ] ),
+        'apellido': new FormControl('', Validators.required)
+      }),
+      'correo': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern('^[a-zA-Z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+                                    ])
+    });
+  }
+
+  guardarCambios(){
+    console.log( this.forma.value );
+    console.log( this.forma );
   }
 
 }
--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+      .ng-invalid.ng-touched:not(form) {
+          border:1px solid red;
+
+      }
+  `]
 })
 export class TemplateComponent {
 
+  usuario:Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    /*pais: "CO"*/
+    pais: "",
+    sexo: "Hombre",
+    acepta: true
+  }
+
+  sexos:string[] = ["Hombre", "Mujer"];
+
+  paises:Object = [
+    {
+      codigo: "CO",
+      nombre: "Colombia"
+    },
+    {
+      codigo: "ES",
+      nombre: "España"
+    },
+    {
+      codigo: "AR",
+      nombre: "Argentina"
+    }
+  ];
+
   constructor() { }
 
-  guardar(){
+  guardar( forma:NgForm ){
     console.log("Formulario posteados")
+    console.log( "NgForm", forma );
+    console.log( "Value: ", forma.value );
+    console.log( "Usuario", this.usuario);
   }
 
 }
