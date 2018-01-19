01291c2103eb026900034bc6187824aa3e717c96
unknown <edwi02@gmail.com>
2018-01-19 03:22:52 +0100

    Validación asincorna

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data',
@@ -53,7 +54,10 @@ export class DataComponent {
                                         Validators.minLength(3)
                                       ] ),
         'apellido': new FormControl('',
-                                        Validators.required)
+                                        [
+                                          Validators.required,
+                                          this.noHerrera
+                                        ])
       }),
       'correo': new FormControl('',
                                    [
@@ -62,17 +66,77 @@ export class DataComponent {
                                     ]),
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required )
-      ])
+      ]),
+      'userName':new FormControl('', Validators.required, this.existeUsuario ),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     });
 
     //Cargar los datos del objeto en el formulario 8Opción 2)
     //this.forma.setValue( this.usuario );
+
+    //Otra forma de establecer validaciones
+    this.forma.controls['password2'].setValidators([
+    Validators.required,
+    this.noIgual.bind( this.forma ) // Por un comportamiento de JavaScript
+                                    // se debe ejecutar como Bind. Esto lo
+                                    // que hace es indicar que "this.forma" que
+                                    // está en otro contexto, ahora es igual a
+                                    //"this "" Eso indica que al nencesitarlo en
+                                    // la función se coloca "this"
+  ])
   }
 
   agregarPasatiempo(){
     (<FormArray>this.forma.controls['pasatiempos']).push(
       new FormControl('', Validators.required )
     )
+  }
+
+  noHerrera( control: FormControl ): { [s:string]:boolean} {
+
+    if( control.value === "herrera"){
+      return {
+        noherrera:true
+      }
+    }
+
+    return null;
+
+  }
+
+  noIgual( control: FormControl ): { [s:string]:boolean} {
+
+    //console.log(this);
+    let forma:any = this;
+
+    // if( control.value !== this.forma.controls['password1'].value ){
+    if( control.value !== forma.controls['password1'].value ){
+      return {
+        noiguales:true
+      }
+    }
+
+    return null;
+
+  }
+
+  existeUsuario( control: FormControl ): Promise <any> | Observable<any>{
+
+    let promesa = new Promise(
+        ( resolve, reject ) =>{
+          setTimeout( ()=>{
+            if( control.value === "strider"){
+              resolve ( {existe:true} )
+            }else{
+              resolve( null )
+            }
+          }, 3000)
+        }
+    )
+
+    return promesa;
+
   }
 
   guardarCambios(){
